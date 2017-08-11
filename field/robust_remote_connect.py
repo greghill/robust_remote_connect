87106a74b4bb202fc514be73ecb114666c15bdd5
Greg D. Hill <greghill@cs.stanford.edu>
2017-08-10 22:01:28 -0500

    simplify by assuming field key is not command restricted on cloud box

--- field/robust_remote_connect.py
+++ field/robust_remote_connect.py
@@ -6,6 +6,8 @@ import sys
 from subprocess import Popen, call, PIPE
 import re
 import ipaddress
+from socket import gethostname
+
 
 parser = argparse.ArgumentParser(description='Establish and try to maitain a remote host tunnel to calling machine')
 
@@ -19,7 +21,7 @@ os.environ["AUTOSSH_GATETIME"] = "0"
 os.environ["AUTOSSH_POLL"] = "60" # poll for a connection every 60 seconds
 
 while True: # in case autossh returns
-    autossh_cmd = ["autossh", args.cloud_username + "@" + str(args.cloud_ip), "-R", "0:localhost:22", "sleep"]
+    autossh_cmd = ["autossh", args.cloud_username + "@" + str(args.cloud_ip), "-R", "0:localhost:22", "-N"]
     autossh_process = Popen(autossh_cmd, stderr=PIPE, universal_newlines=True)
 
     for line in autossh_process.stderr:
@@ -27,8 +29,9 @@ while True: # in case autossh returns
         match = re.search("Allocated port ([0-9]*)", line)
         if match:
             port = match.group(1)
-            set_port_cmd = ['autossh', args.cloud_username + '@' + str(args.cloud_ip), 'set-port --port '+ port]
-            call(set_port_cmd) # autossh so automatically retries on poor connection
+            set_port_cmd = "sh -c 'echo %s > /tmp/remote_connect_ports/%s'" % (port, gethostname())
 
+            set_port_cmd = "autossh %s@%s %s" % (args.cloud_username, str(args.cloud_ip), set_port_cmd)
+            call(set_port_cmd, shell=True) # autossh so automatically retries on poor connection
 
     autossh_process.wait()
